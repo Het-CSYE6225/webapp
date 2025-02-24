a4744e59b1b4518de30e59b27e206f610adaffef
hetparikh2003 <parikh.het@northeastern.edu>
2025-02-24 00:48:58 -0500

    Made some changes to test workflow

--- db/config.js
+++ db/config.js
@@ -6,14 +6,13 @@ const dbPass = process.env.DB_PASS;
 const dbHost = process.env.DB_HOST;
 
 // Create initial Sequelize instance
-let sequelize = new Sequelize(dbName, dbUser, dbPass, {
-    host: dbHost,
-    dialect: 'postgres',
-    logging: false,
-    pool: {
-        idle: 10000
-    }
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  dialect: 'postgres',
+  port: process.env.DB_PORT,
 });
+
 
 async function connectWithDatabaseCreation() {
     try {
