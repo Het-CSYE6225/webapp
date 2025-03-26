df7e7a82bb04bb6494a726e93a5568602eac3a76
hetparikh2003 <parikh.het@northeastern.edu>
2025-03-26 10:15:33 -0400

    Replace StatsD with CloudWatch metrics

--- config/metrics.js
+++ config/metrics.js
@@ -1,11 +1,29 @@
-const StatsD = require('hot-shots');
-const statsd = new StatsD({
-  host: 'localhost',
-  port: 8125,
-  prefix: 'myApplication.',
-  errorHandler: error => {
-    console.error('StatsD error:', error);
-  }
-});
+// /config/metrics.js
+const AWS = require('aws-sdk');
+const cloudwatch = new AWS.CloudWatch({ region: process.env.AWS_REGION });
 
-module.exports = statsd;
+const sendCustomMetric = (name, value, unit = 'Count', serviceName = 'YourServiceName') => {
+    const params = {
+        MetricData: [{
+            MetricName: name,
+            Dimensions: [{
+                Name: 'ServiceName',
+                Value: serviceName
+            }],
+            Timestamp: new Date(),
+            Value: value,
+            Unit: unit
+        }],
+        Namespace: 'YourApplicationMetrics'
+    };
+
+    cloudwatch.putMetricData(params, function(err, data) {
+        if (err) {
+            console.log("Error sending metric to CloudWatch:", err);
+        } else {
+            console.log("Metric successfully sent to CloudWatch:", data);
+        }
+    });
+};
+
+module.exports = { sendCustomMetric };
