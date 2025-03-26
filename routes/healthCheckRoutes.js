8ec330efaf4479f630e2964e2a9edd5b421482a3
hetparikh2003 <parikh.het@northeastern.edu>
2025-03-26 10:23:08 -0400

    Updated the code to measure time of request for healthz

--- routes/healthCheckRoutes.js
+++ routes/healthCheckRoutes.js
@@ -1,7 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const healthCheckController = require('../controllers/healthCheckController.js');
+const logger = require('../config/logger');
+const { sendCustomMetric } = require('../config/metrics'); // Updated to use CloudWatch metrics
 
+router.use((req, res, next) => {
+    const start = Date.now(); // Start time for measuring request duration
+
+    res.on('finish', () => {
+        const duration = Date.now() - start;
+        const routePath = req.route ? req.route.path : req.path;
+
+        sendCustomMetric(`routes.${routePath}.Duration`, duration, 'Milliseconds');
+        sendCustomMetric(`routes.${req.method}.${routePath}.Calls`, 1);
+
+        logger.info(`Request processed: ${req.method} ${routePath} - ${res.statusCode} [${duration}ms]`);
+    });
+
+    next();
+});
+
+// Supported health check routes
 router.get('/healthz', healthCheckController.performHealthCheck);
 router.all('/healthz', healthCheckController.handleUnsupportedMethods);
 
